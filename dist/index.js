0cf68fd12b20b336037585f37deed0a57370f357
Bart van Sleeuwen <bart.vansleeuwen@pidz.nl>
2019-10-03 09:41:25 +0200

    Compiled new binary in dist

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,2 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Command_1 = require("./src/Command");
-const yargs_1 = require("yargs");
-const Arguments_1 = require("./src/lib/Arguments");
-const TsPathCommand = new Command_1.Command();
-TsPathCommand.execute(new Arguments_1.Arguments(yargs_1.argv));
+require('./src/init');
